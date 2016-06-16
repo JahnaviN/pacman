64203f04c26b5d9f18c2c6f3c425d40b29e78132
jahnavi <jahnavi@Jahnavi.Nukireddy>
2016-06-16 22:27:32 +0530

    Corrected pacman movement

--- pacman.py
+++ pacman.py
@@ -8,9 +8,6 @@ def initializeBoard():
 
 	board[7][19] = 'P'
 	board[12][7] = 'G'
-	# board[3][5] = 'G'
-	# board[7][32] = 'G'
-	# board[13][12] = 'G'
 
 	for x in range(20):
 		while 1:
@@ -56,6 +53,8 @@ class Person(object):
 			self.new_x = self.x - 1
 		elif move == 'd':
 			self.new_y = self.y + 1
+		self.new_x = self.new_x%15
+		self.new_y = self.new_y%35
 
 	def checkWall(self, board):
 		if board[self.new_x][self.new_y] == 'X':
@@ -83,16 +82,34 @@ class Pacman(Person):
 		self.score += 1
 
 	def updatePosition(self,board):
+		if board[self.new_x][self.new_y] == 'X':
+			self.new_x = self.x
+			self.new_y = self.y
+			return
 		board[self.x][self.y] = '.'
+		board[self.new_x][self.new_y] = 'P'
 		self.x = self.new_x
 		self.y = self.new_y
-		board[self.x][self.y] = 'P'
 
 class Ghost(Person):
 	
 	def __init__(self,x,y):
 		Person.__init__(self,x,y)
 		self.flag = 0
+
+	def checkPlayer(self,board) :
+		if board[self.new_x][self.new_y] == 'P':
+			return 1
+		return 0
+
+	def updatePosition(self,board):
+		if self.flag == 1:
+			board[self.x][self.y] = 'C'
+		else:
+			board[self.x][self.y] = '.'
+		self.x = self.new_x
+		self.y = self.new_y
+		board[self.x][self.y] = 'G'
 
 if __name__ == '__main__':
 
@@ -104,9 +121,11 @@ if __name__ == '__main__':
 		showBoard(board)
 		move = raw_input("Enter your move : ")
 		if move == 'q' :
+			gameOver(player.score)
 			break
 		elif move not in moves :
 			print "Move not recognized"
+			continue
 		else :
 			player.makeMove(move)
 			if player.checkGhost(board) :
@@ -114,6 +133,21 @@ if __name__ == '__main__':
 				break
 			elif player.checkCoin(board) :
 				player.collectCoin(board)
-			elif player.checkWall(board) :
-				continue
+				if player.score == coinsCount :
+					gameOver(player.score)
+					break
+
 			player.updatePosition(board)
+
+			move = moves[random.randint(0,3)]
+			ghost.makeMove(move)
+
+			if ghost.checkPlayer(board) :
+				gameOver(player.score)
+				break
+			elif ghost.checkCoin(board) :
+				ghost.flag = 1
+				continue
+			elif ghost.checkWall(board) :
+				continue
+			ghost.updatePosition(board)
